c96bac4987ce45e4608562406f38be28b13d053e
johescob_cib <johescob@bancolombia.com.co>
2025-09-22 16:59:34 -0500

    git: new terraform: my-gitops-iac-tofucontroller-ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-06a974f9b8a97ecf2" # Update this with a valid AMI ID
+    ami           = "ami-0ca4d5db4872d0c28" # Amazon Linux 2 AMI #us-east-2
     instance_type = "t2.micro"
     tags = {
-        Name = "OpenTofu-Instance"
+        Name = "Tofu-controller-Instance"
     }
 }
