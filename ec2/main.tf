f7b7608923cf29fbd66455997acb499a15f4b9fe
johescob_cib <johescob@bancolombia.com.co>
2025-09-29 17:29:08 -0500

    change instance name

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
     ami           = "ami-0ca4d5db4872d0c28" # Amazon Linux 2 AMI #us-east-2
     instance_type = "t2.micro"
     tags = {
-        Name = "tofu-controller"
+        Name = "new-tofu-controller"
     }
 }
