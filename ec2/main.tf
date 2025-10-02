131cb5bfb68d1a33e385ffa0a41a80cad689064c
johescob_cib <johescob@bancolombia.com.co>
2025-10-02 15:43:22 -0500

    git: new terraform: my-gitops-iac-tofucontroller-ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,10 +2,15 @@ provider "aws" {
     region = "us-east-2" 
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "instance" {
     ami           = "ami-0ca4d5db4872d0c28" # Amazon Linux 2 AMI #us-east-2
     instance_type = "t2.micro"
     tags = {
-        Name = "observabilidad"
+        Name = "my-gitops-instance"
+        Source = "git:my-gitops-iac-tofucontroller"
     }
+}
+
+output "instance_id" {
+    value = aws_instance.instance.id
 }
