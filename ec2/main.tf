b9c91462c37f34ede1259e6304bb0e1650a5e313
johescob_cib <johescob@bancolombia.com.co>
2026-01-21 14:29:56 -0500

    fix: tags

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,12 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance" {
-    ami           = "ami-0ca4d5db4872d0c28" # Amazon Linux 2 AMI #us-east-2
+    ami           = "ami-0ca4d5db4872d0c28" # Amazon Linux 2 AMI #us-east-2  //ami-058e74ab207ed2b33
     instance_type = "t2.micro"
     tags = {
         Name = "my-gitops-instance"
         Source = "git:my-gitops-iac-tofucontroller"
         appcode = "nu0000000"
+        appname = "tofucontroller"
+        env = "sbx"
     }
 }
 
