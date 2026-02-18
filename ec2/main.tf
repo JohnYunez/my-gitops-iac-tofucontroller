e4a0917ddd651641383a687726001203bb976d10
John Yunez <111716285+JohnYunez@users.noreply.github.com>
2026-02-18 09:57:14 -0500

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "instance" {
     tags = {
         Name = "my-gitops-instance"
         Source = "git:my-gitops-iac-tofucontroller"
-        appcode = "nu0000000"
+        appcode = "nu0445004"
         appname = "tofucontroller"
         env = "sbx"
     }
